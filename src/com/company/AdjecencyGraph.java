19e35c48c516ce5f835a7b0b35cc0e11dfe89282
Hjalte <h.jaltehb123@gmail.com>
2020-11-18 10:05:13 +0100

    updated with comments

--- src/com/company/AdjecencyGraph.java
+++ src/com/company/AdjecencyGraph.java
@@ -18,6 +18,7 @@ public class AdjecencyGraph {
         Edge newEdge = new Edge(from, to, distance);
     }
 
+    // Make sure all vertices has acces to the edges relating them
     public void makeUndirected(){
         for(int v = 0; v < vertices.size(); v++){
             Vertex currentVertex = vertices.get(v);
@@ -30,19 +31,23 @@ public class AdjecencyGraph {
         }
     }
 
+    // Figure out the shortest route around to all vertices
     public void MSTprims(){
         PriorityQueue<Vertex> townDistances = new PriorityQueue<>();
 
+        // Make every vertex be the last priority in the priorityqueue
         for(int v = 0; v < vertices.size(); v++){
             vertices.get(v).distance = Integer.MAX_VALUE;
             vertices.get(v).predecessor = null;
             vertices.get(v).isInList = true;
             townDistances.offer(vertices.get(v));
         }
+        // Declare the vertex we should start from by setting distance
         Vertex startVertex = townDistances.poll();
         startVertex.distance = 0;
         townDistances.offer(startVertex);
 
+        // Remove the shortest distance vertex and assign its edge values to the corresponding vertices
         while(!townDistances.isEmpty()){
             Vertex MSTVertex = townDistances.poll();
             MSTVertex.isInList = false;
@@ -59,6 +64,7 @@ public class AdjecencyGraph {
         }
     }
 
+    // Print out the shortest route around all vertexes
     public void printMST(){
         int totalDistance = 0;
         System.out.println("This is the cheapest route from town to town: ");
